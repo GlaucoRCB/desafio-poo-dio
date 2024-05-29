4231fc0aff2cd3feafab3e6a0e013d62dfd0fca1
glauco <glauquinho2@gmail.com>
2024-05-29 15:07:58 -0300

    Desafio Concluido

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import br.com.dio.desafio.dominio.Conteudo;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
@@ -23,9 +21,42 @@ public class Main {
         mentoria.setDescricao("descrição mentoria java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
+        /*System.out.println(curso1);
         System.out.println(curso2);
-        System.out.println(mentoria);
+        System.out.println(mentoria);*/
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("BootCampDio");
+        bootcamp.setDescricao("Descrição do curso, cheio de conteúdos!");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devGlauco = new Dev();
+        devGlauco.setNome("Glauco");
+        devGlauco.inscreverBootcamp(bootcamp);
+        System.out.println("Contéudos Incritos Glauco" + devGlauco.getConteudosInscritos());
+        devGlauco.progredir();
+        System.out.println("XP:" + devGlauco.calculaTotalXp());
+        devGlauco.progredir();
+        System.out.println("XP:" + devGlauco.calculaTotalXp());
+        System.out.println("-");
+        System.out.println("Contéudos Incritos Glauco" + devGlauco.getConteudosInscritos());
+        devGlauco.progredir();
+        System.out.println("Contéudos Concluídos Glauco" + devGlauco.getConteudosConcluidos());
+        System.out.println("XP:" + devGlauco.calculaTotalXp());
+
+        System.out.println("----------------------------------------");
+
+        Dev devAna = new Dev();
+        devAna.setNome("Ana");
+        devAna.inscreverBootcamp(bootcamp);
+        System.out.println("Contéudos Incritos Ana" + devAna.getConteudosInscritos());
+        devAna.progredir();
+        System.out.println("-");
+        System.out.println("Contéudos Incritos Ana" + devAna.getConteudosInscritos());
+        System.out.println("Contéudos Concluídos Ana" + devGlauco.getConteudosConcluidos());
+        System.out.println("XP:" + devAna.calculaTotalXp());
 
     }
 }
